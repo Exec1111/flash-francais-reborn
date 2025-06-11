4c67ab248891bdc9f11795113fc0de3fca3bc1d7
Exec1111 <julien.vachey@gmail.com>
2025-06-11 14:47:44 +0200

    feat: implémente la page tableau de bord avec statistiques et avertissements

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../services/api'; // Importer l'instance API
+import api from '../services/api'; // Importer l'instance API
 import {
   Box,
   Typography,
